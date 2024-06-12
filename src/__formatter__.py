0f7be2ea59ad51a3da03c0e94d338d3ca41aa836
blurry16 <roma.reznik@gmail.com>
2024-06-13 02:11:01 +0300

    i coded a formatter for scenario yay!

---
+++ src/__formatter__.py
@@ -0,0 +1,31 @@
+# Console app nothing to say about it
+
+with open(
+    r"D:\Everlasting Summer\game\mods\everlasting_internment\src\input.txt",
+    "r",
+    encoding="UTF-8",
+) as file:
+    data = file.readlines()
+
+characters = {
+    "Рома": "rm",
+    "Вика": "vi",
+    "Андрей": "le",
+    "Соня": "sa",
+    "…": "dreamgirl",
+    "...": "dreamgirl"
+}
+
+for index, i in enumerate(data):
+    if "~" == data[index][0]:
+        data[index] = f'th "{i[2:-3]}"\n'
+    elif ":" in i.split()[0]:
+        character = i.split()[0][:-1]
+        if character in characters:
+            data[index] = f'{characters[character]} "{i[len(character) + 2:-1]}"\n'
+        else:
+            print(f"Unknown character! -> {character}")
+    else:
+        data[index] = f'"{i[:-1]}"\n'
+
+print("".join(data))
